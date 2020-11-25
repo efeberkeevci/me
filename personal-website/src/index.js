6a164cba8ed69b908258033b57ae1f40ea83742d
Efe Evci <efeberkeevci@gmail.com>
2020-11-25 16:07:55 +0300

    Switched to react, Started building first page

---
+++ personal-website/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import "bootstrap/dist/css/bootstrap.css";
+import Signature from "./signature";
+
+ReactDOM.render(<Signature />,document.getElementById("root"));
