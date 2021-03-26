fe90a2ac0e4cd8ace6b39f8cf4540be67182f5ef
Efe Evci <efeberkeevci@gmail.com>
2021-03-27 02:22:55 +0300

    Added CNAME file for directing website to efeevci.com

--- personal-website/src/Components/Header.js
+++ personal-website/src/Components/Header.js
@@ -8,7 +8,7 @@ class Header extends Component {
    state = {
       is_mainpage_visible : true,
       is_welcome_page_visible : true,
-      opacity : 0      
+      opacity : 0     
    }
    componentWillUnmount(){this.setState({is_mainpage_visible:true, is_welcome_page_visible:false});}
    switchToMainPage(){setInterval(() => {this.componentWillUnmount()}, 4000);}
@@ -48,28 +48,31 @@ class Header extends Component {
 
     
     if(this.state.is_mainpage_visible){
-      this.mainpage_transition();
+      //this.mainpage_transition();
       return (
          <div id="app_container">
 
-         <header id="home" style = {{opacity : this.state.opacity}}>
+        {/* <header id="home" style = {{opacity : this.state.opacity}}>  */} 
+        <header id="home" >
          
    
          <nav id="nav-wrap">
    
             <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
             <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
-   
+   {/* 
             <ul id="nav" className="nav">
                <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
                <li><a className="smoothscroll" href="#calendar">My Focus</a></li>
                {/*
                
+                  
                <li><a className="smoothscroll" href="#portfolio">Portfolio</a></li>
                <li><a className="smoothscroll" href="#contact">Contact</a></li>
-                              */}
+                              
 
             </ul>
+            */}
    
          </nav>
          
@@ -85,14 +88,17 @@ class Header extends Component {
          </div>  
         
         
-      
+      {/*
          <p className="scrolldown">
             <a className="smoothscroll" href="#calendar"><i className="icon-down-circle"></i></a>
          </p>
+      */}
 
       </header>
+      {/*
       <Calendar></Calendar>
       <Footer></Footer>
+      */}
       </div>
        );
      }
