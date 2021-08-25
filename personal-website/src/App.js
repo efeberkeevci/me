e486d768b631f85b8e7dab65cfd8cc27d5b0c46b
Efe Evci <efeberkeevci@gmail.com>
2021-08-24 17:16:08 -0700

    Combining backend and frontend repositories

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -1,59 +1,50 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
 import './App.css';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import About from './Components/About';
-import Resume from './Components/Resume';
-import Contact from './Components/Contact';
-import Testimonials from './Components/Testimonials';
-import Portfolio from './Components/Portfolio';
-import MainPage from "./mainpage"
-import Calendar from "./Calendar"
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom' 
+import Header from './Components/Header.js';
+
+//import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import ReactGA from 'react-ga';
 
 class App extends Component {
 
 
-  constructor(props){
-    super(props);
-    this.state = {
-      foo: 'bar',
-      resumeData: {}
-    };
+    constructor(props) {
+            super(props);
+            this.state = {
+                foo: 'bar',
+                resumeData: {}
+            };
+        }
+        /*
+        Fetches data from a json file and feeds to sub-components as props
+        */
+    getResumeData() {
+        $.ajax({
+            url: 'resumeData.json',
+            dataType: 'json',
+            cache: false,
+            success: function(data) {
+                this.setState({ resumeData: data });
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.log(err);
+                alert(err);
+            }
+        });
+    }
 
-    //ReactGA.initialize('UA-110570651-1');
-    //ReactGA.pageview(window.location.pathname);
+    componentDidMount() {
+        this.getResumeData();
+        //ReactGA.initialize('UA-191999663-1');
+        //ReactGA.pageview("/homepage");
+    }
 
-  }
+    render() {
+        return ( <
+            Header data = { this.state.resumeData.main }
+            />);
+        }
+    }
 
-  getResumeData(){
-    $.ajax({
-      url:'resumeData.json',
-      dataType:'json',
-      cache: false,
-      success: function(data){
-        this.setState({resumeData: data});
-      }.bind(this),
-      error: function(xhr, status, err){
-        console.log(err);
-        alert(err);
-      }
-    });
-  }
-
-  componentDidMount(){
-    this.getResumeData();
-    ReactGA.initialize('UA-191999663-1');
-    ReactGA.pageview("/homepage");
-  }
-
-  render() {
-    return (
-        <Header data= {this.state.resumeData.main}> </Header>
-    );
-  }
-}
-
-export default App;
+    export default App;
